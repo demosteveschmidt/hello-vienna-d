d3af45e177f8cf1cc89949e85d621765b09b34d6
Stefan Schmidt <sschmidt@pivotal.io>
2023-09-28 10:41:50 +0200

    bring in all code

--- hello-vienna/build.sh
+++ hello-vienna/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cp ../../03-Spring/hello-vienna/target/*SNAP*.jar .
+# cp ../../03-Spring/hello-vienna/target/*SNAP*.jar .
+sdk use java 17.0.8-amzn
+./mvnw package
+
 docker build -t hello-vienna .
